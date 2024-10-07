d4514d99ff248717ec9a0229c057ab4111a6f669
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-10-07 16:27:17 -0400

    Adicionado as configurações de banco de dados, criado a entidade Administradores e realizado as Migrations criando a tabela no banco

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using minimal_api.Dominio.DTOs;
+using minimal_api.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options => {options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql")));});
+
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 
