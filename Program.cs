42fb798479d9dc5ded8f2cbc1afd2d4eca6dbd7e
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-10-07 15:51:34 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+using minimal_api.Dominio.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456"){
+        return Results.Ok("Login com Sucesso");
+    }
+    else
+        return Results.Unauthorized();
+});
+
+
+app.Run();
+
