f48b97ddf599a5f3f2dc9126383a168389f03a3e
Luiz <playsonbr@hotmail.com>
2024-10-07 21:30:34 -0300

    Foi criada a entidade de serviço e toda a sua implementação de servico e endpoints

--- Infraestrutura/Db/DbContexto.cs
+++ Infraestrutura/Db/DbContexto.cs
@@ -14,7 +14,7 @@ namespace minimal_api.Infraestrutura.Db
                 _configuracaoAppSettings = configuracaoAppSettings;
         }
         public DbSet<Administrador> Administradores {get; set;} = default!;
-
+        public DbSet<Veiculo> Veiculos {get; set;} = default!;
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Administrador>().HasData(
